be268f958e75e5f962fc7956e4f07080225a3ded
Wade Lin <wadeclin@gmail.com>
2018-06-10 14:50:22 -0700

    Add Lab 4 to 8

---
+++ lab7/genMino.h
@@ -0,0 +1,13 @@
+#ifndef GENMINO_H
+#define GENMINO_H
+
+#include "Mino.h"
+#include "MinoS.h"
+#include "MinoI.h"
+#include "MinoL.h"
+#include "MinoO.h"
+#include "MinoT.h"
+
+Mino * genMino();
+
+#endif
